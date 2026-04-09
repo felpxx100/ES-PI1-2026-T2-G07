5cb5dcc2d8d5141c7e2e3b02c0c9cf5ae5acb712
felpxx100 <viperfulbox200stro@gmail.com>
2026-04-09 16:07:24 -0300

    Adiciona navegação do módulo de votação e urna

--- index.py
+++ index.py
@@ -35,7 +35,58 @@ match menup:
                 print ("Opção inválida")
 
     case 2:
-        pass
+        print("Escolha uma opção")
+        modulo_vot=int(input(f"\n1- Abrir Sistema de Votação\n2- Auditoria do Sistema de Votação\n3- Resultado da Votação\n4- Sair\n"))
+        match modulo_vot:
+            case 1:
+                ##submenu abrir sistema de votação 
+                print("\n--- Autenticação do Mesário ---")
+                tit_mesario=input(f"Digite o título de eleitor: ")
+                cpf_mesario=input(f"Digite os 4 primeiros dígitos do CPF: ")
+                chv_mesario=input(f"Digite a chave de acesso: ")
+                
+                ##fluxo da zerézima apenas visual
+                print("\n[ Sistema: Zerézima realizada com sucesso. ]")
+                
+                ##menu de operação da urna
+                print("\nEscolha a opção da Urna")
+                menu_urna=int(input(f"\n1- Votar\n2- Encerrar Sistema de Votação\n"))
+                
+                match menu_urna:
+                    case 1:
+                        ##votar
+                        print("\n--- Identificação do Eleitor ---")
+                        tit_eleitor=input(f"Digite o título de eleitor: ")
+                        cpf_eleitor=input(f"Digite os 4 primeiros dígitos do CPF: ")
+                        chv_eleitor=input(f"Digite a chave de acesso: ")
+                        
+                        num_cand=input(f"\nDigite o número do candidato: ")
+                        print("\n[ Sistema: Voto registrado! ]")
+                        pass
+                    case 2:
+                        ##encerrar sistema de votação
+                        print("\n--- Autenticação para Encerramento ---")
+                        tit_mesario_enc=input(f"Digite o título de eleitor do mesário: ")
+                        cpf_mesario_enc=input(f"Digite os 4 primeiros dígitos do CPF: ")
+                        chv_mesario_enc=input(f"Digite a chave de acesso: ")
+                        
+                        confirma=input(f"\nDeseja realmente encerrar a votação? (Sim/Não): ")
+                        print("\n[ Sistema: Votação Encerrada. ]")
+                        pass
+                    case _:
+                        print("Opção inválida")
+            case 2:
+                ##auditoria do sistema de votação 
+                pass
+            case 3:
+                ##resultado da votação
+                pass
+            case 4:
+                ##saída
+                print("Saindo...")
+            case _:
+                print("Opção inválida")
+
     case 3:
         ##saída
         print("Saindo...")
