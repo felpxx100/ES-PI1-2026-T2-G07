f2bbe6c31e8deab13fe2f59c17f8679941813774
Joao <joaoiha2612@gmail.com>
2026-05-16 14:01:38 -0300

    feat: implementa validacao de credenciais no banco de dados

--- registrar log.py
+++ registrar log.py
@@ -12,3 +12,21 @@ def registrar_log(mensagem):
     arquivo = open("logs_urna.txt", "a", encoding="utf-8")
     arquivo.write(f"[{agora}] {mensagem}\n")
     arquivo.close()
+    
+def validar_credenciais(cursor, titulo, cpf_4_digitos, chave, perfil_exigido):
+    # Verifica no banco de dados se as credenciais inseridas estão corretas.
+    cursor.execute(f"SELECT CPF_Eleitor, mesario FROM Eleitores WHERE titulo_eleitoral = '{titulo}' AND chave_acesso = '{chave}'")
+    resultado = cursor.fetchall()
+    
+    if len(resultado) > 0:
+        cpf_banco = resultado[0][0]
+        e_mesario = resultado[0][1]
+        
+        # Verifica se os 4 primeiros dígitos correspondem
+        if cpf_banco[0:4] == cpf_4_digitos:
+            if perfil_exigido == "mesario" and e_mesario == 1:
+                return True
+            elif perfil_exigido == "eleitor":
+                return True
+                
+    return False
